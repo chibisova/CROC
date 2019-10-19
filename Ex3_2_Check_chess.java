e54d17610e7b1d761c2b6c4a42e51982a1646391
TaChibis <38836859+chibisova@users.noreply.github.com>
2019-10-19 17:50:28 +0300

    Create Ex3_2_Check_chess.java
    В виде массива строк задана некоторая последовательность положений на шахматной доске. Например, "b1", "a3", "c4", "d6". Реализовать метод, проверяющий, что последовательность может быть пройдена фигурой конь в соответствии с правилами хода этой фигуры (буквой "Г").
    
    Для этих целей определить новый класс обрабатываемого исключения, которое будет генерироваться методом проверки в случае ошибки. Класс должен содержать информацию о неправильном ходе: из какой в какую клетки ход запрещен.
    
    При вызове метода проверки это исключение должно обрабатываться, а неправильный ход выводиться на экран.
    
    Пример:
    
    [in]  "g8", "e7", "e6"
    [out] "конь так не ходит: e7 -> e6"
    
    [in]  "g8", "e7", "c8"
    [out] "OK"

---
+++ Ex3_2_Check_chess.java
@@ -0,0 +1,6 @@
+package croc.ex_3_2;
+
+public class Check_chess {
+  public Check_chess(String alpha, String alpha_next) throws IllegalArgumentException {
+    throw new IllegalArgumentException("конь так не ходит:" + alpha + " -> " + alpha_next);
+  }
